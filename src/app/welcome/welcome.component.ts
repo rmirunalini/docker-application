5b6587efc03b204d9974cd27c23b77ee7a89c3a3
Mirunalini Ravichandran <rmirunalini@zeomega.com>
2022-01-28 17:02:28 +0530

    dockerapp

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit ,ViewChild ,ElementRef} from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  @ViewChild('name') nameKey!: ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+startTest(){
+  localStorage.setItem("name", this.nameKey.nativeElement.value);
+}
+}
